ff955f01373fbd82e0198c8ebc0bdd1aac9a358b
shahnewaz601 <shahnewaz601@gmail.com>
2021-02-26 23:12:25 +0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [countries, setCountries] =  useState([]);
+ useEffect(() => {
+   fetch("https://restcountries.eu/rest/v2/all")
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      setCountries(data);
+    })
+    .catch(err => console.log("Error"));
+ }, [])
   return (
     <div className="App">
+      <h1>Country Loaded: {countries.length}</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
