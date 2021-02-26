1225c9d531e6bde64f08b6ae2c6ae4e4bfab77b7
shahnewaz601 <shahnewaz601@gmail.com>
2021-02-27 01:34:51 +0600

    add reduce method

--- src/App.js
+++ src/App.js
@@ -1,33 +1,41 @@
 import { useEffect, useState } from 'react';
 import Country from './components/Country/Country';
 import './App.css';
+import Cart from './components/Cart/Cart';
 
   function App() {
     const [countries, setCountries] =  useState([]);
-  useEffect(() => {
-    fetch("https://restcountries.eu/rest/v2/all")
-      .then(res => res.json())
-      .then(data => {
-        setCountries(data);
-        // const names = data.map(country => country.name);
-        // console.log(names);
-      })
-      .catch(err => console.log("Error"));
-  }, [])
-  
-  //OnClick Handler
-  const handleAddCountry = (country) => {
-    console.log('Added', country);
-  }
 
-  return (
-    <div className="App">
-      <h1>Country Loaded: {countries.length}</h1>
-      {
-       countries.map(country => <Country country={country} key={country.alpha3Code} handleAddCountry={handleAddCountry}></Country>)
-      }
-    </div>
-  );
-}
+    useEffect(() => {
+      fetch("https://restcountries.eu/rest/v2/all")
+        .then(res => res.json())
+        .then(data => {
+          setCountries(data);
+          // const names = data.map(country => country.name);
+          // console.log(names);
+        })
+        .catch(err => console.log("Error"));
+    }, [])
+  
+    //OnClick Handler
+    const [cart, setCart] = useState([]);
+
+    const handleAddCountry = (country) => {
+      console.log('Added', country);
+      const newCart = [...cart, country];
+      setCart(newCart);
+    }
+
+    return (
+      <div className="App">
+        <h1>Country Loaded: {countries.length}</h1>
+        <h4>Country Added: {cart.length}</h4>
+        <Cart cart={cart}></Cart>
+        {
+        countries.map(country => <Country country={country} key={country.alpha3Code} handleAddCountry={handleAddCountry}></Country>)
+        }
+      </div>
+    );
+  }
 
 export default App;
